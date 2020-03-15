b7d12fd59045a9910f30a4f7b4bd0df65824a3d9
Jaska8 <jaska@finbits.net>
2020-03-15 11:00:08 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
-find /music/* -iname *.ogg -type f > /config/playlist.txt
+find /music/* -iname "*.ogg" -type f > /config/playlist.txt
 pass="$(date +%s | sha256sum | base64 | head -c 10)"
 sed -i "s/hackme/$pass/g" /config/ices.xml /config/icecast.xml
